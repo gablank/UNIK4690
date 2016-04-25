418f43b84a071f9612488c4b4b7ce990b2eeeffa
Anders Wenhaug <wenhaug@stud.ntnu.no>
2016-04-25 16:31:47 +0200

    Rewrite image_capture

--- image_capture.py
+++ image_capture.py
@@ -1,44 +1,54 @@
 #!/usr/bin/python3
 import cv2
 import datetime
-import subprocess
-
-EAST_CAMERA = ("east", 3)
-SOUTH_CAMERA = ("south", 2)
-cameras = [SOUTH_CAMERA]
 
 
 if __name__ == "__main__":
-    with open("test", "w") as f:
-        f.write("running!")
+    import utilities
+    from camera import Camera
 
-    for folder, camera_idx in cameras:
-        cap = cv2.VideoCapture(camera_idx)
+    image_dir = "/home/anders/UNIK4690/project/images/microsoft_cam/red_balls/"
 
-        if not cap.isOpened():
-            with open("log", "a+") as f:
-                f.write("{}: Unable to open {}!\n".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), folder))
-            continue
+    with Camera() as camera:
+        while True:
+            camera.set(cv2.CAP_PROP_EXPOSURE, 1)
+            frame = camera.capture()
+            key = utilities.show(frame, time_ms=0, text="Exposure: 1")
 
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+            if key == ' ':
+                filename = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.png")
+                cv2.imwrite(image_dir + "1/" + filename, frame)
 
-        ctr = 0
-        ret = False
-        # Capture 50 images before saving so the camera has time to focus
-        while ctr < 50:
-            ret, frame = cap.read()
-            ctr += 1
+            if key == 'n':
+                break
 
-        image_file = "/home/anders/UNIK4690/project/temp.png"
+        while True:
+            camera.set(cv2.CAP_PROP_EXPOSURE, 2)
+            frame = camera.capture()
+            key = utilities.show(frame, time_ms=0, text="Exposure: 2")
 
-        if ret:
-            cv2.imwrite(image_file, frame)
+            if key == ' ':
+                filename = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.png")
+                cv2.imwrite(image_dir + "2/" + filename, frame)
 
-            filename = str(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")) + ".png"
-            ret = subprocess.call(["scp", "-P 22000", image_file, "anders@31.45.53.135:/var/www/unik4690_project/public_html/{}".format(filename)])
-            print(ret)
-            ret = subprocess.call(["ssh", "-p 22000", "anders@31.45.53.135", "/var/www/unik4690_project/link_newest.sh"])
-            print(ret)
+            if key == 'n':
+                break
+
+        while True:
+            camera.set(cv2.CAP_PROP_EXPOSURE, 5)
+            frame = camera.capture()
+            key = utilities.show(frame, time_ms=0, text="Exposure: 5")
+
+            if key == ' ':
+                filename = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.png")
+                cv2.imwrite(image_dir + "5/" + filename, frame)
+
+        #        if ret:
+
+
+#            ret = subprocess.call(["scp", "-P 22000", image_file, "anders@31.45.53.135:/var/www/unik4690_project/public_html/{}".format(filename)])
+ #           print(ret)
+  #          ret = subprocess.call(["ssh", "-p 22000", "anders@31.45.53.135", "/var/www/unik4690_project/link_newest.sh"])
+   #         print(ret)
 
 
