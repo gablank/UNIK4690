02e86ffbdcc024b95f45410f73cc3f5b9b94675d
Anders Wenhaug <awenhaug@gmail.com>
2016-04-27 23:23:41 +0200

    Bug fix and add error header if path is wrong

--- raspberry/picapture.py
+++ raspberry/picapture.py
@@ -7,15 +7,28 @@ import cv2
 
 class CameraCaptureHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-        with camera.Camera() as cam:
-            bgr = cam.capture()
+        if self.path == "/new_image.png":
+            print("received request")
+            with camera.Camera() as cam:
+                bgr = cam.capture()
 
-        cv2.imwrite("image.png", bgr)
-        self.path = "image.png"
-        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET()
+            cv2.imwrite("image.png", bgr)
+            self.path = "image.png"
+            return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+        self.send_error(402)
 
 
 if __name__ == "__main__":
     server = SocketServer.TCPServer(("", 1337), CameraCaptureHandler)
 
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except Exception as e:
+        print(e)
+        import traceback
+        traceback.print_tb(e.__traceback__)
+    finally:
+        server.server_close()
+
+
