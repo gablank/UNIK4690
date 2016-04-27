4430f19364fc31af83d432a9d805bda11409b850
Anders Wenhaug <awenhaug@gmail.com>
2016-04-28 01:26:20 +0200

    Fix setting exposure via URL

--- raspberry/picapture.py
+++ raspberry/picapture.py
@@ -9,11 +9,16 @@ class CameraCaptureHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path.find("/new_image.png") >= 0:
             exposure_pos = self.path.find("&exposure=")
-            exposure = self.path[exposure_pos+len("&exposure="):]
+            exposure = None
+            if exposure_pos >= 0:
+                exposure = int(self.path[exposure_pos+len("&exposure="):])
+                print("Setting exposure to {}".format(exposure))
 
             with camera.Camera() as cam:
-                cam.set(camera.EXPOSURE, exposure)
+                if exposure is not None:
+                    cam.set(camera.EXPOSURE, exposure)
                 bgr = cam.capture()
+                print(cam)
 
             cv2.imwrite("image.png", bgr)
             self.path = "image.png"
