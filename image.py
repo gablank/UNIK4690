gablank/UNIK4690

commit 5b822c76314371ab839f362ec5a75703cd6ef04a

Support for save/load playground adjustment
- ManualPlaygroundDetector uses the saved playground polygon
  or prompts user.
- image.py: keeps path so it's possible to fetch metadata

image.py
@@ -12,12 +12,14 @@ class Image:
 
         self.filename = None
         if path is not None:
-            path = utilities.locate_file(path)
+            self.path = utilities.locate_file(path)
 
             self.bgr = cv2.imread(path)
             self.filename = os.path.basename(path)
             if self.bgr is None:
                 raise FileNotFoundError("Unable to load image from {}".format(path))
+        else:
+            self.path = None
 
         if image_data is not None:
             self.bgr = image_data
@@ -37,6 +39,11 @@ class Image:
         self.lab = None
         self.ycrcb = None
         self.color_space_dict = None
+
+    def get_metadata(self):
+        if self.path is None:
+            return {}
+        return utilities.read_metadata(self.path)
 
     def get_bgr(self, dtype=np.float32):
         return utilities.astype(self.bgr, dtype)

petanque_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from playground_detection.flood_fill import FloodFillPlaygroundDetector
 from ball_detection.minimize_gradients import MinimizeGradientsBallDetector
+from playground_detection.manual_playground_detector import ManualPlaygroundDetector
 from image import Image
 import cv2
 import utilities
@@ -8,6 +9,7 @@ import threading
 import time
 import numpy as np
 import math
+import os
 
 
 class PetanqueDetection:
@@ -193,6 +195,14 @@ class PetanqueDetection:
                     polygon = polygon[mouseover_idx:] + polygon[:mouseover_idx]
                     mouseover_idx = 0
                     pressed_idx = 0 if pressed_idx is not None else None
+            elif key == 'd':
+                if image.path:
+                    # Assume image serie
+                    utilities.update_metadata(os.path.dirname(image.path), {"playground_poly": polygon})
+            elif key == 'l':
+                metadata = image.get_metadata()
+                if "playground_poly" in metadata:
+                    polygon = [tuple(coord) for coord in metadata["playground_poly"]]
 
             with userdata["lock"]:
                 userdata["run"] = run

playground_detection/manual_playground_detector.py
@@ -1,0 +1,17 @@
+import utilities
+
+
+class ManualPlaygroundDetector:
+    def __init__(self, petanque_detection):
+        pass
+
+    def detect(self, image):
+        metadata = image.get_metadata()
+
+        if "playground_poly" in metadata:
+            return [tuple(coord) for coord in metadata["playground_poly"]]
+        else:
+            # user will be prompted to adjust polygon and can save it for later with 'd'
+            return [(100,100), (100,200), (200,200), (200,100)]
+            # playground_poly = utilities.select_polygon(image.get_bgr())
+            # metadata = utilities.update_metadata(image.path, {"playground_poly": playground_poly})

commit 2f653b40dda0fc8179ef93f6747d7eb5fb7b5ce2

Bugfix: Fix Image path lookup broken by previous commit
Regression from 5b822c76314371ab839f362ec5a75703cd6ef04a

image.py
@@ -12,14 +12,14 @@ class Image:
 
         self.filename = None
         if path is not None:
-            self.path = utilities.locate_file(path)
+            path = utilities.locate_file(path)
 
             self.bgr = cv2.imread(path)
             self.filename = os.path.basename(path)
             if self.bgr is None:
                 raise FileNotFoundError("Unable to load image from {}".format(path))
-        else:
-            self.path = None
+
+        self.path = path
 
         if image_data is not None:
             self.bgr = image_data