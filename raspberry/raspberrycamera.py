51d5ed606fbd0fcc7f08fc6c69562d93935e1208
Anders Wenhaug <awenhaug@gmail.com>
2016-04-30 16:04:49 +0200

    Add support for setting settings in PiCamera

--- raspberry/raspberrycamera.py
+++ raspberry/raspberrycamera.py
@@ -15,15 +15,23 @@ class RaspberryCamera(camera.Camera):
     def __init__(self):
         self._camera_device = "Raspberry Pi camera"
 
-        self._frame_width = None
-        self._frame_height = None
+        self._frame_width = 1920
+        self._frame_height = 1080
+
+        self._settings = {}
+
+        self._get_settings()
 
         super(RaspberryCamera, self).__init__()
 
     def capture(self):
         stream = io.BytesIO()
         with picamera.PiCamera() as cam:
+            # Apply settings
             cam.resolution = (self._frame_width, self._frame_height)
+            for setting, value in self._settings.items():
+                setattr(cam, setting, value)
+
             cam.start_preview()
             # Let the camera warm up
             time.sleep(2)
@@ -33,10 +41,12 @@ class RaspberryCamera(camera.Camera):
         return bgr
 
     def set(self, property, value):
-        pass
+        self._settings[property] = value
 
     def get(self, property):
-        return 0
+        if property in self._settings:
+            return self._settings[property]
+        return None
 
     def set_resolution(self, width, height):
         self._frame_width = int(width)
@@ -44,6 +54,25 @@ class RaspberryCamera(camera.Camera):
 
     def get_resolution(self):
         return self._frame_width, self._frame_height
+
+    def _get_settings(self):
+        with picamera.PiCamera() as cam:
+            self._frame_width, self._frame_height = cam.resolution
+            self._settings["contrast"] = cam.contrast
+            self._settings["sharpness"] = cam.sharpness
+            self._settings["brightness"] = cam.brightness
+            self._settings["zoom"] = cam.zoom
+            self._settings["shutter_speed"] = cam.shutter_speed
+            self._settings["saturation"] = cam.saturation
+            self._settings["exposure_compensation"] = cam.exposure_compensation
+
+    def __str__(self):
+        as_str = "Camera read from: {}\n".format(self._camera_device)
+        as_str += "Camera settings:\n"
+        as_str += "Resolution: {}x{}\n".format(self._frame_width, self._frame_height)
+        for setting, value in self._settings.items():
+            as_str += setting.capitalize() + ": {}\n".format(value)
+        return as_str
 
     def __enter__(self):
         return self
