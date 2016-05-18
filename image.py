da798bdfd2cc57babb48fe5093e6bb8e7f24e2f8
Anders Wenhaug <wenhaug@stud.ntnu.no>
2016-05-18 13:25:04 +0200

    Raise error if invalid histogram equalization is selected

--- image.py
+++ image.py
@@ -24,22 +24,26 @@ class Image:
         if image_data is not None:
             self.bgr = image_data
 
-        if histogram_equalization == "rg_chromaticity":
-            # rg chromaticity: normalized rgb
-            bgr = self.get_bgr()
-            S = bgr[:,:,0] + bgr[:,:,1] + bgr[:,:,2]
-            for i in range(3):
-                bgr[:,:,i] *= 255/S
-                self.bgr[:,:,i] = bgr[:,:,i].astype(np.uint8)
+        if histogram_equalization is not None:
+            if histogram_equalization == "rg_chromaticity":
+                # rg chromaticity: normalized rgb
+                bgr = self.get_bgr()
+                S = bgr[:,:,0] + bgr[:,:,1] + bgr[:,:,2]
+                for i in range(3):
+                    bgr[:,:,i] *= 255/S
+                    self.bgr[:,:,i] = bgr[:,:,i].astype(np.uint8)
 
-        elif histogram_equalization == "target_average":
-            self.bgr = utilities.as_float32(self.bgr)
-            target_averages = [0.636815, 0.543933, 0.469305]
-            for i in range(3):
-                self.bgr[:,:,i] += target_averages[i] - np.average(self.bgr[:,:,i])
-            self.bgr[np.where(self.bgr > 1.0)] = 1.0
-            self.bgr[np.where(self.bgr < 0.0)] = 0.0
-            self.bgr = utilities.as_uint8(self.bgr)
+            elif histogram_equalization == "target_average":
+                self.bgr = utilities.as_float32(self.bgr)
+                target_averages = [0.636815, 0.543933, 0.469305]
+                for i in range(3):
+                    self.bgr[:,:,i] += target_averages[i] - np.average(self.bgr[:,:,i])
+                self.bgr[np.where(self.bgr > 1.0)] = 1.0
+                self.bgr[np.where(self.bgr < 0.0)] = 0.0
+                self.bgr = utilities.as_uint8(self.bgr)
+
+            else:
+                raise ValueError("histogram_equalization must be one of None, rg_chromaticity and target_average")
 
         self.hsv = None
         self.lab = None
